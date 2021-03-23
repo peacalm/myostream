4e0b7cca7b51242c46e348395fecfe17ea56d765
lishuangquan <lishq991@gmail.com>
2021-03-23 16:43:03 +0800

    copyright symble using (c) instead of ©
    Change-Id: I061dd06b12fa4df3cc871e7d46082aa191c46c97

--- include/myostream.h
+++ include/myostream.h
@@ -1,4 +1,4 @@
-// Copyright © 2021 Shuangquan Li. All Rights Reserved.
+// Copyright (c) 2021 Shuangquan Li. All Rights Reserved.
 //
 // Licensed under the MIT License (the "License"); you may not use this file
 // except in compliance with the License. You may obtain a copy of the License
