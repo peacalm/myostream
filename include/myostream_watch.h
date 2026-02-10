fd73bd1fc03df16cb2803618a67fb2794d6c87e3
Li Shuangquan <lishq991@gmail.com>
2026-02-11 00:43:18 +0800

    Add myostream_watch.h

---
+++ include/myostream_watch.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 Shuangquan Li. All Rights Reserved.
+//
+// Licensed under the MIT License (the "License"); you may not use this file
+// except in compliance with the License. You may obtain a copy of the License
+// at
+//
+//   http://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+// =============================================================================
+
+#ifndef MYOSTREAM_WATCH_H_
+#define MYOSTREAM_WATCH_H_
+
+#include <iostream>
+
+#include "myostream.h"
+
+#ifndef DISABLE_MYOSTREAM_watch
+
+#define watch(...)                        \
+  std::cout << MYOSTREAM_WATCH_TO_STRING( \
+      std::string, " = ", "\n", "\n\n", __VA_ARGS__)
+
+#endif
+
+#endif  // MYOSTREAM_WATCH_H_
